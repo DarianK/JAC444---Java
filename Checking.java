f7ecf960bbae2bf220a9b83831bb2268f273c7dd
Darian K <dariankoarga@gmail.com>
2018-05-31 19:45:34 -0400

    Create Checking.java
    Code pertains to a Checking account subclass to Account superclass. Overrides withdraw method by enabling an overdraw limit of up to $1000.

---
+++ Checking.java
@@ -0,0 +1,20 @@
+/**
+Submitted By: Darian Koarga
+Course: JAC444
+Date: May 31, 2018
+Id: 136296167
+Contains Task 4, Checking Account Implementation
+*/
+public class Checking extends Account {
+  private double overdraft = -1000; //account can be overdrawn by up to $1000
+  Checking(int id_, double balance_) {super(id_, balance_);} //invoke superclass constructor to create a checking account
+  public double withdraw(double amount) { //withdraw specified amount from account
+    if((super.getBalance() - amount) < overdraft) {
+      System.out.println("You have exceeded your overdraw limit of $1000");
+      return super.getBalance();
+    }
+    else {
+      return super.withdraw(amount);
+    }
+  }
+}
